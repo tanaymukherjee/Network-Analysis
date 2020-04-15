49115140eb725021c3e8436e50b6a43f18b35168
Tanay Mukherjee <its_tanay_here@yahoo.com>
2020-04-14 23:43:04 -0400

    Add files via upload

---
+++ Network Analysis.R
@@ -0,0 +1,13 @@
+# load the libraries
+library(tidyverse)
+library(tidygraph)
+library(ggraph)
+
+# Use tbl_graph
+library("navdata")
+data("phone.call2")
+phone.net <- tbl_graph(
+  nodes = phone.call2$nodes, 
+  edges = phone.call2$edges,
+  directed = TRUE
+)
